d8dce506e987edf4682be1d1c595c8bee3b4d9fc
Akbar Maloer (ridzen) <93141972+Ridzen@users.noreply.github.com>
2022-01-18 21:54:15 +0800

    Update Exam2 task _4.py

--- Exam2 task _4.py
+++ Exam2 task _4.py
@@ -4,5 +4,5 @@ def func(*args):
     result = 0
     for n in args:
         if isinstance(n, numbers.Number):
-            result = result+n
-    return result
+ #           result = result+n
+            return result
